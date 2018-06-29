48ed08964107d17045bd4085e9de8ad0c9358c25
Christine Stawitz <cstawitz@uw.edu>
2018-06-29 16:28:29 -0700

    more updates

--- analysis.R
+++ analysis.R
@@ -16,7 +16,7 @@ source("R/vast_config.R")
 #Read in raw data
 raw_data <- read.csv("./data/EBSLengths.csv")
 renames <- c('Year', 'station',
-             'Lat','Lon','Area_Swept_km2',
+             'Lat','Lon','AreaSwept_km2',
              'length')
 
 
@@ -27,8 +27,16 @@ data_plan <- drake_plan(
   arrowtooth = create_data(raw_data, species=10110, sex=2, age=4)
 )
 
+Data_Geostat<- create_data(raw_data,species=21740,
+                           sex=2, age=7, renames)
+Extrapolation_List = SpatialDeltaGLMM::Prepare_Extrapolation_Data_Fn( Region=Region, strata.limits=strata.limits )
 
-Spatial_List = SpatialDeltaGLMM::Spatial_Information_Fn( grid_size_km=grid_size_km, n_x=n_x, Method=Method, Lon=Data_Geostat[,'Lon'], Lat=Data_Geostat[,'Lat'], Extrapolation_List=Extrapolation_List, randomseed=Kmeans_Config[["randomseed"]], nstart=Kmeans_Config[["nstart"]], iter.max=Kmeans_Config[["iter.max"]], DirPath=DateFile, Save_Results=FALSE )
+Spatial_List = SpatialDeltaGLMM::Spatial_Information_Fn( grid_size_km=grid_size_km, n_x=n_x, Method=Method, 
+                                                         Lon=Data_Geostat[,'Lon'], Lat=Data_Geostat[,'Lat'], 
+                                                         Extrapolation_List=Extrapolation_List, 
+                                                         randomseed=Kmeans_Config[["randomseed"]], 
+                                                         nstart=Kmeans_Config[["nstart"]], iter.max=Kmeans_Config[["iter.max"]], 
+                                                         DirPath=DateFile, Save_Results=FALSE )
 # Add knots to Data_Geostat
 Data_Geostat = cbind( Data_Geostat, "knot_i"=Spatial_List$knot_i )
 TmbData = Data_Fn("Version"=Version, "FieldConfig"=FieldConfig, "OverdispersionConfig"=OverdispersionConfig, 
