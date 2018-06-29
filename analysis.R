caf919e5aebabd33c7ad97ff20d328db781388e7
Christine Stawitz <cstawitz@uw.edu>
2018-06-29 14:18:36 -0700

    clean up analysis script

--- analysis.R
+++ analysis.R
@@ -1,7 +1,7 @@
 raw_data <- read.csv("./data/EBSLengths.csv")
 require(dplyr)
+
 pollock <- raw_data %>% filter(SPECIES_CODE==21740) %>% filter(Sex==2) %>% filter(age==7)
-require(dplyr)
 renames <- c('year', 'station',
                   'lat','long','area_swept','age',
                   'length', 'weight')
