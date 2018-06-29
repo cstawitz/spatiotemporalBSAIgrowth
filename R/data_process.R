aef6321032995a149847aa76a3b44224a2fd2db9
Christine Stawitz <cstawitz@uw.edu>
2018-06-29 14:52:35 -0700

    don't hardcode in length as response variable

--- R/data_process.R
+++ R/data_process.R
@@ -5,8 +5,9 @@
 #'@param dataset__ = data.frame containing raw data
 #'@param renames = list of new names
 #'@param id.vars = names of columns to construct a unique ID for
+#'@param response = name of the column that contains your response variable (i.e. length, catch)
 #'@return a tibble that can be input into VAST
-data_process<- function(dataset__,  renames, id.vars, ...){
+data_process<- function(dataset__,  renames, id.vars, response, ...){
   #Subset to only useful columns and rename them
   qs <- rlang::quos(...)
   subsetted <- select(dataset__,rlang::UQS(qs))
@@ -19,9 +20,10 @@ data_process<- function(dataset__,  renames, id.vars, ...){
   #Group by unique ID and calculate average length and weight
   DF <- munged %>%
     group_by(ID) %>%
-    mutate(avg_length=mean(length), avg_weight=mean(length)) %>%
-    select(-length, -weight) %>%
-    distinct()
+    mutate(Catch_KG=mean(!!!rlang::sym(response)), Vessel=0) %>%
+    select(-response) %>%
+    distinct() %>%
+    ungroup()
   return(DF)
   
 }
--- analysis.R
+++ analysis.R
@@ -1,10 +1,10 @@
 raw_data <- read.csv("./data/EBSLengths.csv")
 require(dplyr)
 
-pollock <- raw_data %>% filter(SPECIES_CODE==21740) %>% filter(Sex==2) %>% filter(age==7)
-renames <- c('year', 'station',
-                  'lat','long','area_swept','age',
-                  'length', 'weight')
+pollock <- raw_data %>% filter(SPECIES_CODE==21740) %>% filter(Sex==2) %>% filter(AGE==7)
+renames <- c('Year', 'station',
+                  'Lat','Lon','Area_Swept_km2',
+                  'length')
 
-data_process(pollock, renames, id.vars=c("age", "station", "year"),
-             YEAR, STATIONID, START_LATITUDE, START_LONGITUDE, AREA_SWEPT..km.2., AGE, LENGTH..cm., WEIGHT..g.)
+data_process(pollock, renames, id.vars=c("station", "Year"), response="length",
+             YEAR, STATIONID, START_LATITUDE, START_LONGITUDE, AREA_SWEPT..km.2., LENGTH..cm.)
