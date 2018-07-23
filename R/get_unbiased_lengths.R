7eead21a2872ac017565f916b4ba8eb9da82a3ae
Christine Stawitz <cstawitz@uw.edu>
2018-07-23 10:04:49 -0700

    add to function

--- R/get_unbiased_lengths.R
+++ R/get_unbiased_lengths.R
@@ -6,18 +6,22 @@
 #'@return a matrix of mean ages per year
 get_unbiased_lengths <- function(dataset__, age.name, length.name, year.name, station.name){
   #initialize dimension scalars
-  browser()
   
   l <- round(select(dataset__,length.name),0)
   length_groups <- min(l):max(l)
   ages <- min(select(dataset__,age.name), na.rm=T):max(select(dataset__,age.name), na.rm=T)
-  age_lengths <- array(dim=c(length(length_groups), length(ages)+1))
-  bystation_yr<- dataset__ %>% filter(!!sym(station.name)!="") %>%
-    group_by(!!sym(year.name), !!sym(station.name), !!sym(age.name), round(!!sym(length.name),0)) %>%
-    summarise(n())
-
-  get_each_station_yr <- function(station_yr_df,age_lengths){
   
+  bystation_yr<- dataset__ %>% filter(!!sym(station.name)!="") %>%
+    mutate("ID"=paste(!!sym(year.name), !!sym(station.name), sep="_")) %>%
+    group_by(ID, !!sym(age.name), "r.length" = round(!!sym(length.name),0)) %>%
+    summarise("sample.size"=n())
+
+
+  get_each_station_yr <- function(station_yr_df){
+    age_lengths <- array(dim=c(length(length_groups), length(ages)+1))
+    browser()
+    x <- station_yr_df %>% purrr::map(~ age_lengths[which(length_groups==.$r.length),which(ages==.$AGE)] <- .$sample.size)
+
   #Do math
   Nj <- apply(age_lengths,1,sum)
   nj <- apply(age_lengths[,-(ages+1)],1,sum)
@@ -31,6 +35,10 @@ get_unbiased_lengths <- function(dataset__, age.name, length.name, year.name, st
     unbiased_l[i,] <- sum(Nij*avgLength[i,])/Ni[i]    
   }
   }
+  
+  
+  x <- split(bystation_yr, bystation_yr$ID) %>%
+    purrr::map(~get_each_station_yr(.x))
   
   
 }
