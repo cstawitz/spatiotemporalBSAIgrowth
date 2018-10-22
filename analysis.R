08bcafe7e7e3bf4d25e4062c19991e35d7d31956
Christine Stawitz <cstawitz@uw.edu>
2018-10-22 16:24:45 -0700

    diagnostics

--- analysis.R
+++ analysis.R
@@ -60,9 +60,9 @@ Report = Obj$report()
 Save=list("Opt"=Opt, "Report"=Report, "ParHat"= Obj$env$parList(Opt$par), "TmbData"=TmbData)
 save(Save, file=paste0(DateFile, "Save.RData"))
 
-cod.obj <- load("Save.RData")
+load("./VAST_Output/Save.RData")
+Report <- Save$Report
 plot_data(Extrapolation_List, Spatial_List, Data_Geostat,PlotDir=DateFile)
-
 
 
 ## Convergence
@@ -71,9 +71,9 @@ pander::pandoc.table( Opt$diagnostics[,c('Param','Lower','MLE','Upper','final_gr
 
 ## Diagnostics for positive-catch-rate component
 
-Q = plot_quantile_diagnostic( TmbData=TmbData, Report=Report, FileName_PP=paste0(DateFile,"Posterior_Predictive.jpg"),
-                             FileName_Phist=paste0(DateFile,"Posterior_Predictive-Histogram.jpg"), 
-                             FileName_QQ=paste0(DateFile,"Q-Q_plot.jpg"), FileName_Qhist=paste0(DateFile,"Q-Q_hist.jpg"))
+Q = plot_quantile_diagnostic( TmbData=TmbData, Report=Report, FileName_PP="Posterior_Predictive.jpg",
+                             FileName_Phist="Posterior_Predictive-Histogram.jpg", 
+                             FileName_QQ="Q-Q_plot.jpg", FileName_Qhist="Q-Q_hist.jpg")
 
 
 ## Diagnostics for plotting residuals on a map
@@ -91,6 +91,7 @@ plot_anisotropy( FileName=paste0(DateFile,"Aniso.png"), Report=Report, TmbData=T
 
  Report <- Save$Report
  Opt <- Save$Opt
+ ParHat <- Save$ParHat
 Dens_xt = plot_maps(plot_set=c(3), MappingDetails=MapDetails_List[["MappingDetails"]], Report=Report, Sdreport=Opt$SD, PlotDF=MapDetails_List[["PlotDF"]], MapSizeRatio=MapDetails_List[["MapSizeRatio"]], Xlim=MapDetails_List[["Xlim"]], Ylim=MapDetails_List[["Ylim"]], FileName=DateFile, Year_Set=Year_Set, Years2Include=Years2Include, Rotate=MapDetails_List[["Rotate"]], Cex=MapDetails_List[["Cex"]], Legend=MapDetails_List[["Legend"]], zone=MapDetails_List[["Zone"]], mar=c(0,0,2,0), oma=c(3.5,3.5,0,0), cex=1.8, plot_legend_fig=FALSE)
 
 Dens_DF = cbind( "Density"=as.vector(Dens_xt), "Year"=Year_Set[col(Dens_xt)], "E_km"=Spatial_List$MeshList$loc_x[row(Dens_xt),'E_km'], "N_km"=Spatial_List$MeshList$loc_x[row(Dens_xt),'N_km'] )
